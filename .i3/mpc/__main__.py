f98952fde1d7f7e5120be755904e1caeb5c05210
Guillaume Subiron <maethor@subiron.org>
2015-02-07 19:22:10 +0100

    Added i3 configuration

---
+++ .i3/mpc/__main__.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim: ai ts=4 sts=4 et sw=4 nu
+
+from __future__ import (unicode_literals, absolute_import,
+                        division, print_function)
+
+import os
+import sys
+import notify2
+
+
+default_cover = os.environ['HOME'] + "/.i3/mpc/default_cover.jpg"
+COVER_SIZE = 75
+music_folder = os.environ['HOME'] + "/Musique"
+
+
+def next_track():
+    os.popen("mpc next")
+    notify()
+
+def prev_track():
+    os.popen("mpc prev")
+    notify()
+
+def play_pause():
+    os.popen("mpc toggle")
+    notify()
+
+def get_cover():
+    filename = os.popen("mpc -f '%file%'").readlines()[0][:-1]
+    folder = music_folder + '/' + os.path.dirname(filename)
+    covers = filter(lambda x: x.endswith('.jpg') or x.endswith('.png') or x.endswith('.gif'), os.listdir(folder))
+    if covers:
+        return (folder + '/' + covers[0])
+    else:
+        return default_cover
+
+def notify():
+    i = dict()
+    i['track'] = os.popen("mpc -f '%track%'").readlines()[0][:-1]
+    i['title'] = os.popen("mpc -f '%title%'").readlines()[0][:-1]
+    i['album'] = os.popen("mpc -f '%album%'").readlines()[0][:-1]
+    i['artist'] = os.popen("mpc -f '%artist%'").readlines()[0][:-1]
+
+    misc = os.popen("mpc").readlines()[1][:-1]
+    if "playing" in misc:
+        i['status'] = misc.replace("playing", "<span color='#73d216'>playing</span>")
+    elif "paused" in misc:
+        i['status'] = misc.replace("paused", "<span color='#f57900'>paused</span>")
+    else:
+        i['status'] = misc
+
+    # This is ugly
+    if misc[-2] == '%':
+        if misc[-4] != '(':
+            i['progress'] = int(misc[-4:-2])
+        else:
+            i['progress'] = int(misc[-3])
+
+    msg = "%(track)s - %(title)s\n%(artist)s - %(album)s\n%(status)s" % i
+
+    notify2.init('MPC')
+    n = notify2.Notification("Now playing:", msg, get_cover())
+    n.set_hint('progress', 80) # Doesn't work with dunst :(
+    n.show()
+    
+    pass
+
+if __name__ == '__main__':
+    if len(sys.argv) < 1:
+        print("Error not enough arguements")
+    else:
+        command = sys.argv[1]
+        if command == 'play_pause':
+            play_pause()
+        elif command == 'next':
+            next_track()
+        elif command == 'prev':
+            prev_track()
+        elif command == 'notify':
+            notify()
---
+++ .i3/statusbar.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+#import subprocess
+
+from i3pystatus import Status
+import netifaces
+
+green = "#8ae234"
+yellow = "#fce94f"
+orange = "#f57900"
+red = "#ef2929"
+fg = "#aaaaaa"
+gray = "#aaaaaa"
+
+status = Status(standalone=True)
+
+status.register("clock", format="🕗 %a %d/%M %X",)
+
+status.register("battery",
+                format="🔋 {percentage:.1f}% {remaining:%E%h:%M}",
+                alert=True, alert_percentage=5,
+                status={ "DIS": "↓", "CHR": "↑", "FULL": "=", },
+                not_present_text = "N/A",
+                color=fg,
+                full_color=green,
+                charging_color=yellow,
+                critical_color=red,
+                not_present_color=gray,
+                )
+
+#status.register("disk", path="/", format="{used}/{total}G [{avail}G]",)
+
+status.register("alsa", format="🔈 {volume}%", increment=2)
+
+status.register("cpu_usage", format="CPU {usage:02}%")
+
+status.register("mem",
+                format="MEM {used_mem:.0f}MiB/{total_mem:.0f}MiB",
+                color="#ffffff",
+                warn_percentage=70, alert_percentage=90)
+
+status.register("temp", format="{temp:.0f}°C")
+
+#status.register("network_traffic", interface="wlan0")
+
+base_net_format_up = "{v4cidr}"
+interfaces = netifaces.interfaces()
+
+for interface in interfaces:
+    if interface.startswith('wlan'):
+        # Wireless network
+        status.register("wireless", interface=interface,
+                        format_up="{essid} ({quality:.2f}%): " + base_net_format_up,
+                        color_up=green,color_down=red,)
+    elif interface.startswith('eth'):
+        # Wired network
+        status.register("network", interface=interface, format_up=base_net_format_up,
+                        color_up=green,color_down=red,)
+    elif interface.startswith('tun'):
+        # Wired network
+        status.register("network", interface=interface, format_up="VPN "+base_net_format_up,
+                        color_up=green,color_down=red, unknown_up=True)
+
+# FDN VPN
+#status.register("network", interface=interface,
+#                format_up="FDN: {v6cidr} {v4cidr}",
+#                format_down="FDN: down",
+#                unknown_up=True,)
+#
+#status.register("network",
+#    interface="eth0",
+#    format_up="{v4cidr}",)
+#
+#status.register("wireless",
+#    interface="wlan0",
+#    format_up="{essid} {quality:03.0f}%",)
+
+# Shows mpd status
+# Format:
+# Cloud connected▶Reroute to Remain
+status.register("mpd",
+    format="{status} {album} - {title}",
+    status={
+        "pause": " ▷ ",
+        "play": " ▶ ",
+        "stop": " ◾ ",
+    },)
+
+status.run()
