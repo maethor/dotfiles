c645e0f670f528402bad2c9b48a42f2832cb7672
Guillaume Subiron <maethor@subiron.org>
2022-04-06 08:09:13 +0200

    cleanup-git.sh

---
+++ .bin/cleanup-git.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+rm -rf .bin .git .gnupg .i3 .muttrc.d .ncmpcpp .textmf .urxvt .vim .zprezto .ackrc .bashrc .forward .gitconfig .gitignore .gitmodules .muttrc .profile .screenrc .signature .tmux.conf .vimrc .Xdefaults .zlogin .zpreztorc .zprofile .zshenv .zshrc
